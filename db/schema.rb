8bcca6cc9ebb554cd5bbe1286b6e4351068b712b
Ju777 <julien.tamil@gmail.com>
2022-09-02 13:58:25 +0200

    Add column location to Articles table .

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_01_183849) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_02_115130) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.decimal "price"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "location"
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
   create_table "jwt_denylist", force: :cascade do |t|
     t.string "jti", null: false
     t.datetime "exp", null: false
@@ -31,4 +42,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_183849) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
