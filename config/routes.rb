e774a0b3276a8b955a08dd6111b3b30a0c16490a
Ju777 <julien.tamil@gmail.com>
2022-09-07 11:19:59 +0200

    Test update user + active storage.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   resources :messages
   resources :articles
- 
+  # resources :users
+
   devise_for :users,
              controllers: {
                sessions: 'users/sessions',
                registrations: 'users/registrations'
              }
   get '/member-data', to: 'members#show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/visitors', to: 'members#index'
+  get '/visitors/:id', to: 'members#show_visitors'
+  put '/update_user', to:'members#update'
+  
+  
 end
